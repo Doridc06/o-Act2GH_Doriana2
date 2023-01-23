03789d1d334c8480d4a992a2f249e8e905f35489
Doriana <a_dorianaangelica.dacostamagello@iespablopicasso.es>
2023-01-23 14:04:09 +0100

    Añadir array

---
+++ ActGit_Array/src/Git_array.java
@@ -0,0 +1,18 @@
+
+public class Git_array {
+  public static void main(String[] args) {
+
+
+    final int LONGITUDNUMEROS = 20;
+    final int INICIO = 1;
+    final int FIN = 10;
+
+    int[] numeros = new int[LONGITUDNUMEROS];
+    
+    for (int i = 0; i < LONGITUDNUMEROS; i++) {
+      numeros[i] = (int) (Math.random() * (FIN - INICIO + 1) + INICIO);
+    
+    System.out.println(numeros[i]);
+    }
+  }
+}
